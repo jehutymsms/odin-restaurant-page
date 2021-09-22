6d7b69c628799060cf412b5a8f72e39d14684a24
Andrew Humphries <andrewhumphrieswork@outlook.com>
2021-09-22 07:42:26 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './style.css';
+// What Needs to be done
+// Create a bare-bones homepage for a restaurant. 
+// Include an image, headline, and some copy about how wonderful the restaurant is. 
+// It’s okay to hard-code these into the HTML for now just to see how they look on the page.
+
+console.log('Does this work')
