6b9b945ea0c87c067467de597ca43a6bd2313153
Andrew Humphries <andrewhumphrieswork@outlook.com>
2021-09-23 07:18:00 -0500

    CSS Styling for Nav bar

--- src/index.js
+++ src/index.js
@@ -3,5 +3,3 @@ import './style.css';
 // Create a bare-bones homepage for a restaurant. 
 // Include an image, headline, and some copy about how wonderful the restaurant is. 
 // It’s okay to hard-code these into the HTML for now just to see how they look on the page.
-
-console.log('Does this work')
