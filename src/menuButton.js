ead79d9a4cf472d0f7233619e274cc1feed29e11
Andrew Humphries <andrewhumphrieswork@outlook.com>
2021-10-13 07:58:32 -0500

    Updating function for menu list rendering

--- src/menuButton.js
+++ src/menuButton.js
@@ -3,12 +3,47 @@
 //Home button function should create the elements with a function
 
 export const menuButton = (() =>{
+    // Menu
+    let menuList = {
+        menuTitles:['Drink','Food'],
+        drink:{
+            item0:{
+                title: 'Ale',
+                descript: 'It\'s Better than water'
+            },
+            item1:{
+                title: 'Wine',
+                descript: 'Red'
+            },
+            item2:{
+                title: 'Cider',
+                descript: 'Made from apples that have not rotted'
+            }
+        },
+        food:{
+            item0:{
+                title: 'Stew',
+                descript: 'Meat Vegetables and Broth'
+            },
+            item1:{
+                title: 'Roast Goose',
+                descript: 'Butchered Fresh to Order'
+            },
+            item2:{
+                title: 'Fancy Feast',
+                descript: 'Finest Meat, Cheese, Fruits, and Nuts'
+            },
+            item3:{
+                title: 'Commoner\'s Feast',
+                descript: 'Whatever is left of the Fancy Feast'
+            }
+        }
+    }
     // Cache Dom
     const cacheDom = (()=>{
         let contentBody = document.getElementById('content')
         return {contentBody:contentBody}
-    })()
-    
+    })()  
     // Function List
     const newElement = (item)=>{
         let element = document.createElement(item.tag);
@@ -29,63 +64,68 @@ export const menuButton = (() =>{
 
     }
 
-    const menuDetailElements = () =>{
+    // return a list of Elements based on Menu array
+    const menuDetailElements = (array) =>{
         let elementList = [];
 
-        for(let i =0; i< menuList.menuTitles.length ;i++){
-            let test = newElement({tag:'h1', htmlString:menuList.menuTitles[i]})
-            elementList.push(test)
+        for(let i =0; i< array.menuTitles.length ;i++){
+            let element = newElement({tag:'h1', htmlString:array.menuTitles[i]}),
+            detailItems = menuDetailItems(array[array.menuTitles[i].toLowerCase()])
+            elementList.push(element)
+            // Modify this to loop over object 
+            for(let x = 0;x< detailItems.length;x++){
+                elementList.push(detailItems[i])
+            }
         }
 
         return elementList
     }
 
-    let menuList = {
-        menuTitles:['Drink','Food'],
-        drink:{
-            item1:{
-                title: 'Ale',
-                descript: 'It\'s Better than water'
-            },
-            item2:{
-                title: 'Wine',
-                descript: 'Red'
-            },
-            item3:{
-                title: 'Cider',
-                descript: 'Made from apples that have not rotted'
-            }
-        },
-        food:{
-            item1:{
-                title: 'Stew',
-                descript: 'Meat Vegetables and Broth'
-            },
-            item2:{
-                title: 'Roast Goose',
-                descript: 'Butchered Fresh to Order'
-            },
-            item3:{
-                title: 'Fancy Feast',
-                descript: 'Finest Meat, Cheese, Fruits, and Nuts'
-            },
-            item4:{
-                title: 'Commoner\'s Feast',
-                descript: 'Whatever is left of the Fancy Feast'
-            }
+    //Returns and array of Item Elements
+    const menuDetailItems = (array) =>{
+        // possible return an object to solve duplication problem
+        // console.log(Object.keys(array).length)
+        let list = [],
+        list2 = {};
+        for(let i =0; i < Object.keys(array).length; i++){
+            let details = array[`item${i}`],
+            container = newElement({
+                tag:'div',
+                }),
+            title = newElement({
+                tag:'p',
+                classId:'itemTitle',
+                htmlString:details.title
+            }),
+            descript = newElement({
+                tag:'p',
+                htmlString:details.descript
+            })
+            // console.log(title.innerHTML)
+            container.appendChild(title)
+            // console.log(descript.innerHTML)
+            container.appendChild(descript)
+            list.push(container)
+            list2[`item${i}`] = container;
         }
+        console.log(list2)
+        return list
     }
 
     const menuCreate = () =>{
         menuContentSection();
-        let list = menuList.menuTitles[0].toLowerCase()
+        let list = menuList.menuTitles[0].toLowerCase(),
+        menuDetails = menuDetailElements(menuList),
+        contentBody = document.getElementById('contentBody')
+        
+        for(let i =0; i<menuDetails.length; i++){
+            render.insertInside(menuDetails[i], contentBody)
+        }
+        // console.log(menuList[list][`item1`].title)
+
         // console.log(list);
         // console.log(Object.keys(menuList[menuList.menuTitles[0].toLowerCase()]));
-        // console.log(children[1]);
-        for(let i =0; i<menuDetailElements().length; i++){
-            render.insertAfter(menuDetailElements()[i], document.getElementById('contentBody'))
-        }
-        
+        // console.log(children[1]);  
     }
 
     // Render to DOM
@@ -93,13 +133,11 @@ export const menuButton = (() =>{
         const insertAfter = (newNode, existingNode) =>{
             existingNode.parentNode.insertBefore(newNode, existingNode.nextSibling);
         }
-        return{insertAfter:insertAfter}
+        const insertInside = (element, existingElement) => {
+            existingElement.appendChild(element)
+        }
+        return{insertAfter:insertAfter,insertInside:insertInside}
     })()
-
-    let contentBody = document.getElementById('content'),
-    // use this with insert after function for menu creation
-    children = contentBody.children
-
 
     return {menuCreate:menuCreate}
 })()
